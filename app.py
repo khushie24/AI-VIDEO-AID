e9a7945d859384db83400d5543aa666eb699d721
khushie24 <ktanna780@gmail.com>
2026-06-14 20:28:44 +0530

    Fix import statement for summarizer module

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 from dotenv import load_dotenv
 from utils.audio_processor import process_input
 from core.transcriber import transcribe_all
-from core.summarizer import summarize, generate_title
+from core.summarize import summarize, generate_title
 from core.extractor import extract_action_items, extract_key_decisions, extract_questions
 from core.rag_engine import build_rag_chain, ask_question
 
